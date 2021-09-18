ee2f908c35fda3fbe73ce03eff38cb2d0d38db45
kalki-24 <mek17002@byui.edu>
2021-09-18 16:58:58 -0600

    Updated

--- js/date.js
+++ js/date.js
@@ -1,4 +1,4 @@
 let Lm= document.lastModified; 
 let Y= new Date().getFullYear(); 
-document.getElementById("Modified").innerHTML = Lm; 
+document.getElementById("Updated").innerHTML = Lm; 
 document.getElementById("Year").innerHTML = Y; 

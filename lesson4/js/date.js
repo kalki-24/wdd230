bd4a58e1a320ce153c396c2932597591457c08ac
kalki-24 <mek17002@byui.edu>
2021-10-09 03:41:43 -0600

    update

---
+++ lesson4/js/date.js
@@ -0,0 +1,7 @@
+let x= document.lastModified; 
+let Y= new Date().getFullYear(); 
+let today = new Date();
+let day = today.getDay();
+let daylist = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
+document.getElementById("Updated").innerHTML = daylist[day] + x; 
+document.getElementById("Year").innerHTML = Y; 

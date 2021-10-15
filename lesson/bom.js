2afebb8818216562b03b3166ac6e86a49a3f9bd5
kalki-24 <mek17002@byui.edu>
2021-10-14 20:52:50 -0600

    update

---
+++ lesson/bom.js
@@ -0,0 +1,24 @@
+const list = document.querySelector('ul');
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+
+button.onclick = function() {
+  let favchap = input.value;
+  input.value = '';
+
+  const favchapList = document.createElement('li');
+  const favchapText = document.createElement('span');
+  const deleteBtn = document.createElement('button');
+
+        favchapList.appendChild(favchapText);
+  favchapText.textContent = favchap;
+  favchapList.appendChild(deleteBtn);
+  deleteBtn.textContent = 'Delete';
+  list.appendChild(favchapList);
+
+  deleteBtn.onclick = function(e) {
+    list.removeChild(favchapList);
+  }
+
+  input.focus();
+}

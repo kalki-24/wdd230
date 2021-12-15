5190fb23954eed62b6851f5a1d31616151decbdc
kalki-24 <mek17002@byui.edu>
2021-12-15 03:24:27 -0700

    update

--- final-webpage/js/date.js
+++ final-webpage/js/date.js
@@ -1,4 +1,10 @@
-let x= document.lastModified; 
+
+function toggleMenu() {
+    console.log(document.getElementById("primaryNav").classList);
+    document.getElementById("primaryNav").classList.toggle("hide");  
+}
+
+let Lm= document.lastModified; 
 let Y= new Date().getFullYear(); 
-document.getElementById("Updated").innerHTML = x; 
+document.getElementById("Modified").innerHTML = Lm; 
 document.getElementById("Year").innerHTML = Y; 
---
+++ final-webpage/js/local.js
@@ -0,0 +1,58 @@
+const jsonFile = 'js/local.json';
+const resultElement = document.getElementById('cards')
+
+fetch(jsonFile)
+  .then(function (response) {
+    return response.json(); 
+  })
+  .then(function (jsonObject) {
+    console.log(jsonObject); 
+
+    const businesses = jsonObject['businesses'];
+    let card = document.createElement('section');
+
+
+    for ( let i = 0; i < businesses.length; i++) {
+
+        if (businesses[i].name == "Ambasader Hotel" || businesses[i].name == "Capital Hotel" || businesses[i].name == "Haile Hotel") {
+            let image = document.createElement('img')
+
+            let h2 = document.createElement('h2');
+            let div = document.createElement("div")
+            div.setAttribute('class', "division")
+            h2.setAttribute('class', "name")
+            let h5 = document.createElement('h5');
+            h5.setAttribute('class', "name")
+            let article = document.createElement('article');
+            article.setAttribute('class', "name")
+            let p = document.createElement('p');
+            p.setAttribute('class', "name")
+            let p2 = document.createElement('p');
+            p.setAttribute('class', "name")
+            let p3 = document.createElement('p');
+
+            image.setAttribute('src', 'images/' + businesses[i].photo);
+            image.setAttribute('class', "photos" )
+            image.setAttribute('alt', 'Images depicting the towns')
+            image.apphend = businesses[i].photo
+
+            h2.textContent = businesses[i].name;
+            h5.textContent = businesses[i].motto; 
+            p.textContent = "Price: " + businesses[i].price; 
+            p2.textContent = "Website: " + businesses[i].website;
+            p3.textContent = "Annual Rainfall: " + businesses[i].averageRainfall;
+            
+            div.appendChild(h2);
+            div.appendChild(h5);
+            div.appendChild(p)
+            div.appendChild(p2)
+            div.appendChild(p3)
+            article.appendChild(div)
+            article.appendChild(image)
+            card.appendChild(article); 
+        }
+
+    }
+    resultElement.appendChild(card); 
+
+}); 
